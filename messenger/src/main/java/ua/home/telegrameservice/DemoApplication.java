8e65ba7e26fa6af105711ad2ca56577e169f0975
Andrey <azatvornitskiy@techmatesgroup.com>
2020-07-03 16:47:52 +0300

    initial commit

--- messenger/src/main/java/ua/home/telegrameservice/DemoApplication.java
+++ messenger/src/main/java/ua/home/telegrameservice/DemoApplication.java
@@ -2,16 +2,16 @@ package ua.home.telegrameservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 public class DemoApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
